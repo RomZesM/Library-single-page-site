fb34d83a8200c43be6d4c7d039a3ef9e496e46aa
RomZesM <dwarf.md@gmail.com>
2023-08-13 18:45:46 +0200

    fix: fix script, add selfcheck

--- library/scripts/index.js
+++ library/scripts/index.js
@@ -1,13 +1,13 @@
 "strict mode"
+console.log('Вёрстка соответствует макету. Ширина экрана 768px +26');
+console.log('Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12');
+console.log('На ширине экрана 768рх реализовано адаптивное меню +12');
+
+
 const burger = document?.querySelector('.burger_icon'); //get .class from DOM
 const nav_menu = document?.querySelector('.header__navigation');
 const body = document?.querySelector('body');
 const nav_items = nav_menu?.querySelectorAll('a'); //taka all links from menu list
-
-
-//console.log(burger);
-console.log("----------------------------------------------")
-console.log(nav_items);
 
 burger.addEventListener('click', () => { //when we CLICK on burger
 	nav_menu?.classList.toggle('navigation--visible'); //add some another class
