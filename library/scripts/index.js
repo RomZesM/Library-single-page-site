10dc77ea77843ae67cccfe04506bb26f03510764
RomZesM <dwarf.md@gmail.com>
2023-08-13 17:31:34 +0200

    feat:add burger menu functionality

--- library/scripts/index.js
+++ library/scripts/index.js
@@ -9,7 +9,49 @@
 // console.log("4. Общие требования к верстке +20\n")
 
 
+// function hello(){
+// 	console.log("Hello!");
+// 	addTagsClickHandler();
+// 	getColletion();
+// 	//getColletion2();
+// }
 
+// const addTagsClickHandler = () =>{
+// 	document.querySelector('.about__carousel, .about__images').addEventListener('click', (e) => {
+// 		console.log(e);
+// 	})
+// }
+
+// function getColletion(){
+// 	for(let i = 0; i < document.body.childNodes.length; i++){
+// 		console.log(document.body.childNodes[i]);
+// 	}
+// }
+// function getColletion2(){
+// 	for(let i = 0; i < document.body.header.childNodes.length; i++){
+// 		console.log(document.body.header.childNodes[i]);
+// 	}
+// }
+
+
+//window.onload = hello();
+
+const burger = document?.querySelector('.burger_icon'); //get .class from DOM
+const nav_menu = document?.querySelector('.navigation')
+const body = document?.querySelector('body')
+
+
+console.log(burger);
+console.log("----------------------------------------------")
+console.log(nav_menu);
+
+burger.addEventListener('click', () => { //when we CLICK on burger
+	nav_menu?.classList.toggle('navigation--visible'); //add some another class
+													//in css make navigation-visible display=none	
+	burger?.classList.toggle('burger--active-animate');
+
+	body?.classList.toggle('stop-scroll') //add for body to stop scroll while menu is visible
+});
 
 
 
