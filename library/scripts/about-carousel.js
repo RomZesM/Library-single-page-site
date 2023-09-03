d1fa325e40640d6c38cda615711e11bce7db2122
RomZesM <dwarf.md@gmail.com>
2023-09-03 16:38:08 +0200

    feat: add about pagination button functional (disable function and color changing)

--- library/scripts/about-carousel.js
+++ library/scripts/about-carousel.js
@@ -1,65 +1,99 @@
 ///carousel
+
+import { getSingleDomElementByClass } from "./utils.js";
+
+
+	
+const track = document.querySelector('.about__images');
+// const button01 = document.querySelector('.carousel-b1');
+// const button02 = document.querySelector('.carousel-b2');
+// const buttonCenter03 = document.querySelector('.carousel-b3');
+// const button04 = document.querySelector('.carousel-b4');
+// const button05 = document.querySelector('.carousel-b5');
+// const arrowLeft = document.querySelector('.left_arrow_ico')
+// const arrowRight = document.querySelector('.right_arrow_ico')
+
+const allButtons = getSingleDomElementByClass('about').getElementsByTagName("button")
+
+const gapBetweenPic = 25;
+const itemWidth = 450;	
+let position = 0; //start position
+
 export function createCarousel(){
-	let position = 0; //start position
-	let gapBetweenPic = 25;
-	//const slidesToShow = 3;
-	const itemWidth = 450;
-	//const slidesToScroll = 1;
-
-	//const container = document.querySelector('.about-images_container');
-	const track = document.querySelector('.about__images');
-	//const itemsPictures = document.querySelectorAll('.about__images-item');
-	const button01 = document.querySelector('.carousel-b1');
-	const button02 = document.querySelector('.carousel-b2');
-	const buttonCenter03 = document.querySelector('.carousel-b3');
-	const button04 = document.querySelector('.carousel-b4');
-	const button05 = document.querySelector('.carousel-b5');
-	const arrowLeft = document.querySelector('.left_arrow_ico')
-	const arrowRight = document.querySelector('.right_arrow_ico')
-
-	//const itemCount = itemsPictures.length; //get a quantity of pictures 
-	//const itemWidth = (container.clientWidth - gapBetweenPic * 2) / slidesToShow; //calculate image size (cotainer - gap)
 	
-	button01.addEventListener('click', (event) =>{
-		position = (itemWidth * 2) + gapBetweenPic * 2;
-		track.style.transform = `translateX(${position}px)`;		
+	
+	allButtons.namedItem("carousel-b1-id").addEventListener('click', (event) =>{
+		// position = (itemWidth * 2) + gapBetweenPic * 2;
+		
+		moveTrack(2,1)		
+		enableAllButtonsExceptitself(allButtons.namedItem("carousel-b1-id"));
+		disableLeftEdgeButtons();
 	});
 
-	button02.addEventListener('click', (event) =>{
-		position = (itemWidth) + gapBetweenPic;
-		track.style.transform = `translateX(${position}px)`;		
+	allButtons.namedItem("carousel-b2-id").addEventListener('click', (event) =>{
+		// position = (itemWidth) + gapBetweenPic;
+		
+		moveTrack(1,1)
+		enableAllButtonsExceptitself(allButtons.namedItem("carousel-b2-id"));
 	});
 
-	buttonCenter03.addEventListener('click', (event) =>{
-		position = 0;
-		track.style.transform = `translateX(${position}px)`;		
+	allButtons.namedItem("carousel-b3-id").addEventListener('click', (event) =>{
+		// position = 0;
+		
+		moveTrack(0,1)
+		enableAllButtonsExceptitself(allButtons.namedItem("carousel-b3-id"));
 	});
 
-	button04.addEventListener('click', (event) =>{
-		position = -(itemWidth + gapBetweenPic);
-		track.style.transform = `translateX(${position}px)`;
+	allButtons.namedItem("carousel-b4-id").addEventListener('click', (event) =>{
+		// position = -(itemWidth + gapBetweenPic);
+		
+		moveTrack(1,-1)
+		enableAllButtonsExceptitself(allButtons.namedItem("carousel-b4-id"));
 	});
 
-	button05.addEventListener('click', (event) =>{
-		position = -(itemWidth * 2 + gapBetweenPic * 2);
-		track.style.transform = `translateX(${position}px)`;
+	allButtons.namedItem("carousel-b5-id").addEventListener('click', (event) =>{
+		// position = -(itemWidth * 2 + gapBetweenPic * 2);
+		
+		moveTrack(2,-1)	
+		enableAllButtonsExceptitself(allButtons.namedItem("carousel-b5-id"));
+		disableRightEdgeButtons();
 		
 	});
-
 	
-	arrowLeft.addEventListener('click', (event) => {
+	allButtons.namedItem("left_arrow_ico-id").addEventListener('click', (event) => {
 		if(position < 950){
-			position = position + 475
-		}		
-		track.style.transform = `translateX(${position}px)`;
+			position = position + 475;
+			track.style.transform = `translateX(${position}px)`;
+			disableRoundButton();
+			console.log("position: " + position);//!del
+		}
+		if(position === 950){
+			disableLeftEdgeButtons();
+		}
+		// else{
+		// 	enableAllButtonsExceptitself(null);
+		// }
+				
+		
 		
 	});
 
-	arrowRight.addEventListener('click', (event) => {
+	allButtons.namedItem("right_arrow_ico-id").addEventListener('click', (event) => {
 		if(position > -950){
 			position = position - 475
+			track.style.transform = `translateX(${position}px)`;
+			disableRoundButton();
+			console.log("position: " + position);//!del
 		}
-		track.style.transform = `translateX(${position}px)`;
+		//disable/enable edge buttons
+		if(position === -950){
+			disableRightEdgeButtons();
+		}
+		// else{
+		// 	enableAllButtonsExceptitself(null);
+		// }
+
+		
 		
 	});
 
@@ -77,4 +111,59 @@ export function createCarousel(){
 	});
 
 	
+}
+
+function enableAllButtonsExceptitself(itself){
+	
+		for (let i = 0; i < allButtons.length; i++) {
+			const element = allButtons[i];
+			element.disabled = false;
+			element.classList.remove('carusel-button-current');
+		}
+	
+	if(itself != null){		
+		//disable itself
+		itself.disabled = true;
+		itself.classList.add('carusel-button-current');
+	}
+}
+
+function moveTrack(multiplier, sign){
+	position = sign * ((itemWidth * multiplier) + gapBetweenPic * multiplier);
+	track.style.transform = `translateX(${position}px)`;
+	console.log("position: " + position);//!del
+}
+
+function disableRightEdgeButtons(){
+	allButtons.namedItem("carousel-b5-id").disabled = true;
+	allButtons.namedItem("right_arrow_ico-id").disabled = true;
+}
+function disableLeftEdgeButtons(){
+	allButtons.namedItem("carousel-b1-id").disabled = true;
+	allButtons.namedItem("left_arrow_ico-id").disabled = true;
+}
+
+function disableRoundButton(){
+	switch (position) {
+		case 950:
+			console.log('----------------- 1 -------------');
+			enableAllButtonsExceptitself(allButtons.namedItem("carousel-b1-id"));
+			break;
+		case 475:
+			console.log('----------------- 2 -------------');
+			enableAllButtonsExceptitself(allButtons.namedItem("carousel-b2-id"));
+			break;
+		case 0:
+			console.log('----------------- 3 -------------');
+			enableAllButtonsExceptitself(allButtons.namedItem("carousel-b3-id"));
+			break;
+		case -475:
+			console.log('----------------- 4 -------------');
+			enableAllButtonsExceptitself(allButtons.namedItem("carousel-b4-id"));
+			break;
+		case -950:
+			console.log('----------------- 5 -------------');
+			enableAllButtonsExceptitself(allButtons.namedItem("carousel-b5-id"));
+			break;	
+	}
 }
